9d28f41481c41e75319b5f9734e8e913a90a9c41
sultan <sultanauezov26@gmail.com>
2026-03-17 11:54:28 +0500

    newfile

---
+++ lab7/task13.java
@@ -0,0 +1,16 @@
+package lab7;
+
+public class task13 {
+    public static void main(String[] args) {
+        int[] C = new int[20];
+        int sum = 0;
+
+        for (int i = 0; i < 20; i++) {
+            C[i] = (int)(Math.random() * 100);
+            System.out.print(C[i] + " ");
+            sum += C[i];
+        }
+
+        System.out.println("\nОрта мән: " + (double)sum / 20);
+    }
+}
---
+++ lab7/task5.java
@@ -0,0 +1,16 @@
+package lab7;
+
+public class task5 {
+    public static void main(String[] args) {
+        double[] D = new double[17];
+        double sum = 0;
+
+        for (int i = 0; i < 17; i++) {
+            D[i] = Math.random() * 100;
+            System.out.print(D[i] + " ");
+            sum += D[i];
+        }
+
+        System.out.println("\nОрта мән: " + sum / 17);
+    }
+}
